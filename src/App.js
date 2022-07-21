bafe96c0f4c3831d5dbd603e41b25c28930ddd14
brian blake <104259527+babyslikk@users.noreply.github.com>
2022-07-20 21:24:09 -0500

    Complete Entry Chart Tracker Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React, { useState } from "react";
+import DisplayEntries from "./Components/DisplayEntries/DisplayEntries";
+import AddEntryForm from "./Components/AddEntry/AddEntryFrom";
+import EntriesChartTracker from "./EntriesChartTracker/EntriesChartTracker";
 
 function App() {
+  const [entries, setEntries] = useState([
+    { weight: 175, date: "11-23-2021" },
+    { weight: 176, date: "11-24-2021" },
+  ]);
+
+  function addNewEntry(entry) {
+    let tempEntries = [...entries, entry];
+
+    setEntries(tempEntries);
+  }
+
   return (
     <div>
-      <h3>Hello World</h3>
+      <DisplayEntries parentsEntries={entries} />
+      <AddEntryForm addNewEntry={addNewEntry} />
+      <EntriesChartTracker parentsEntries={entries} />
     </div>
   );
 }
